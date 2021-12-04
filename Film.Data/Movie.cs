be5210e5d5dc22346606bc358cc1bed6bf0d98e9
Savannah Jackson <savannahjackson33@gmail.com>
2021-12-04 12:23:53 -0500

    movie

---
+++ Film.Data/Movie.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Film.Data
+{
+    public enum MovieMaturityRating
+    {
+        G = 1,
+        PG,
+        PG_13,
+        R,
+        NC_17,
+        TV_MA,
+        NR
+    }
+
+    public enum MovieGenre
+    {
+        TV_Drama,
+        Comedy,
+        Factual,
+        Entertainment,
+        Sports,
+        MusicTv,
+        Children,
+        Horror,
+        Thriller,
+        Romantic_Comedy,
+        Drama,
+        Action,
+        Romance,
+        Documentary,
+        Fantasy,
+        Western,
+        Sci_Fi,
+        Musical,
+        Crime,
+        Historical
+    }
+
+    public class Movie
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public double StarRating { get; set; }
+        public MovieMaturityRating MovieMaturityRating { get; set; }
+        public MovieGenre TypeOfGenre { get; set; }
+
+        public Movie(string title, string description, double starRating, MovieMaturityRating movieMaturityRating, MovieGenre genreType)
+        {
+            Title = title;
+            Description = description;
+            StarRating = starRating;
+            MovieMaturityRating = movieMaturityRating;
+            TypeOfGenre = genreType;
+        }
+    }
+}
